5149f9b3708a50c8ab78ac5c3089e416a90c766f
mohdazaan <mohdazaan293@gmail.com>
2026-02-04 16:57:59 +0530

    get, add and delete avaialability

--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,6 @@
 export const config = {
     userDetails : import.meta.env.VITE_BASE_URL + 'user/',
-    login : import.meta.env.VITE_BASE_URL + 'auth/login'  ,
-    register : import.meta.env.VITE_BASE_URL + 'auth/register',
+    login : import.meta.env.VITE_BASE_URL + 'auth/login',
     availability : import.meta.env.VITE_BASE_URL + 'availability'
 }
 
