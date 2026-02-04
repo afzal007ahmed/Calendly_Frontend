359d8112bf809f3f33abcc1bde267b44635074ce
Afzal <afzalahmeddc@gmail.com>
2026-02-04 18:12:44 +0530

    fixes

--- src/RouteManager/ProtectedRoute.jsx
+++ src/RouteManager/ProtectedRoute.jsx
@@ -1,14 +1,22 @@
 import { AppContext } from "@/context/AppContext";
+import { routes } from "@/Routes/routes";
 import { Loader2 } from "lucide-react";
 import React, { useContext } from "react";
+import { Navigate } from "react-router";
 
 const ProtectedRoute = ({ children }) => {
   const { user } = useContext(AppContext);
   const token = localStorage.getItem("token");
+
   if (!user.data || !token) {
-    return <div className="min-h-full flex items-center justify-center" >
+    return <Navigate to={routes.login} />;
+  }
+  if (user.loading) {
+    return (
+      <div className="min-h-full flex items-center justify-center">
         <Loader2 className="animate-spin" size={30} />
-    </div>;
+      </div>
+    );
   }
   return children;
 };
