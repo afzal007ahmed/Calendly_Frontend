c4f17b7bfc70bc0530dbe1151d283bf19976e845
John Richard <John@gmail.com>
2026-02-06 12:40:59 +0530

    redux store updated

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,14 +1,12 @@
-import { configureStore } from "@reduxjs/toolkit"
-import ScheduleReducer from "./scheduleSlice"
-import availabilityReducer from "./availabilitySlice"
+import { configureStore } from "@reduxjs/toolkit";
+import ScheduleReducer from "./scheduleSlice";
+import availabilityReducer from "./availabilitySlice";
+import meetingReducer from "./meetingSlice";
 
 export const store = configureStore({
-    reducer : {
-        ScheduleReducer,
-        availabilityReducer
-    } 
-})
-
-
-
-
+  reducer: {
+    ScheduleReducer,
+    availabilityReducer,
+    meeting: meetingReducer,
+  },
+});
